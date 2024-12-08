a136a537dc026ae9238e0d24fd1d649501651443
Christopher Kumm <chrislkumm@protonmail.com>
2024-12-07 22:51:40 -0600

    Move `src/` directory into new project
    My goal with this commit is to give a bit of a 'refresh' to this project. I tried updating React to version 19 while keeping most of the project intact, but there were many development dependencies that were also out of date. It would have been very difficult to bring everything up to date. Instead, I opted to extract the source files into a new project.
    
    To make this commit, I started by deleting everything except these files:
    
    - Everything in the `src/` directory
    - `README.md`
    - `package.json`
    - `.prettierrc
    - `LICENSE` and `LICENSE.md`
    
    I then transferred in files from the `tsx-react` branch of my [rollup-starter-lib](https://github.com/ChrisCrossCrash/rollup-starter-lib/tree/03604ca48d6a8e206858a410dd1304b567965555). This starter actually only uses `tsc`, and not Rollup, as it did in older versions.
    
    I modified `package.json` by keeping informational metadata like `name`, but deleted or replaced other properties.
    
    This commit still doesn't build correctly, but I wanted to include this as a starting point before changing the original source code. One of the next few commits should build correctly.

--- .storybook/theme.js
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming';
-
-export const theme = create({
-  base: 'light',
-  brandTitle: 'react-chartjs-2',
-  brandUrl: 'https://github.com/reactchartjs/react-chartjs-2',
-});
